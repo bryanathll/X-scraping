246ab6f07f089787a0beebd90dacff19192f1e25
bryanathll <bryannatanael77@gmail.com>
2024-04-10 06:24:44 +0700

    update controller

--- routes/twitter/twitter.controller.js
+++ routes/twitter/twitter.controller.js
@@ -8,10 +8,17 @@ const getTweets = (req, res) =>{
 };
 
 const postTweets = (req, res)=>{
-    const {keyword} = req.body
-    const search = keyword;
-    console.log(search)
-    res200("success get keyword", keyword, res)
+   const {keyword} = req.body
+    scrapingX(keyword, (data)=>{
+        res200("success get data", data, res)
+     });
 }
+
+// const postTweets = (req, res)=>{
+//     const {keyword} = req.body
+//     const search = keyword;
+//     console.log(search)
+//     res200("success get keyword", keyword, res)
+// }
 
 module.exports = {getTweets, postTweets}
